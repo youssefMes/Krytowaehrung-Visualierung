4e953c06e3b283fe4d19fb642f3a54f87f7372a6
youssefMes <youssef-mestiri@outlook.fr>
2021-09-04 13:40:38 +0100

    get asset OHLC && Assets Prices

--- server/routes/main.route.js
+++ server/routes/main.route.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const cryptoController = require('../controllers/main.controller')
 
-router.route('/asset/get-one')
-    .get(cryptoController.getOne);
+router.route('/asset/ohlc')
+    .get(cryptoController.getAssetOHLC);
+router.route('/asset/info')
+    .get(cryptoController.getAssetsPrice);
 
 module.exports = router
