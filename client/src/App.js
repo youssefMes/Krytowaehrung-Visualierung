b3c5248ab011ce619a989b2ac119c540781435ff
Maher <66023807+skylervale@users.noreply.github.com>
2021-09-04 14:50:54 +0100

    theme implementation
    - implemented material ui
    - added components
    - design

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect } from 'react';
 import socketIOClient from "socket.io-client";
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {
+  BrowserRouter as Router,
+  Switch,
+  useHistory,
+  Route
+} from "react-router-dom";
+
+//Style Components
+import './style/main.style.css';
+
+//Cofig Components
+import {useStyles} from './config/usestyles.config';
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import {
+  red,
+  lightBlue,
+  deepPurple,
+  blueGrey
+} from "@material-ui/core/colors";
+
+//Html components
+import {Copyright} from './components/copyright/copyright.component';
+import {HeaderBar} from './components/appbar/appbar.component';
+
 const ENDPOINT = "http://localhost:9000";
 
-function App() {
+function MainApp() {
+  const classes = useStyles();
+  const [searchText, setSearchText] = useState('');
+  const [darkState, setDarkState] = useState(
+    localStorage.getItem('darkState') || false
+  );
+
   const [response, setResponse] = useState("");
 
   useEffect(() => {
@@ -14,11 +44,66 @@ function App() {
     });
   }, []);
 
+  const palletType = darkState ? "dark" : "light";
+  const mainPrimaryColor = darkState ? red[500] : lightBlue[500];
+  const mainSecondaryColor = darkState ? blueGrey[900] : deepPurple[500];
+  const darkTheme = createTheme({
+    palette: {
+      type: palletType,
+      primary: {
+        main: mainPrimaryColor
+      },
+      secondary: {
+        main: mainSecondaryColor
+      }
+    }
+  });
+
+  const displayAlt = darkState ? "Switch to Light Mode" : "Switch to Dark Mode";
+
+  const [filter, setFilter] = useState({})
+
+  const handleFilterChange = (newFilter) => {
+    setFilter(newFilter);
+  };
+
+  const handleThemeChange = () => {
+    localStorage.setItem('darkState', !darkState);
+    setDarkState(!darkState);
+  };
+
   return (
-      <p>
-        It's <time dateTime={response}>{response}</time>
-      </p>
+    <Router>
+    <React.Fragment>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <div className={classes.root}>
+          <HeaderBar classes={classes} setSearchText={setSearchText} onFilterChange={handleFilterChange} filter={filter} colorSwitch={handleThemeChange} darkState={darkState} displayAlt={displayAlt}/>
+        </div>
+        <main>
+          <Switch>
+
+            {/*** LANDING PAGE ***/}
+            <Route path="/">
+              <div>
+                
+              </div>
+ 
+            </Route>
+            {/*** END LANDING PAGE ***/}
+
+          </Switch>
+        </main>
+        {/* Footer */}
+        <footer className={classes.footer}>
+          <Copyright />
+        </footer>
+        {/* End footer */}
+      </ThemeProvider>
+    </React.Fragment>
+     </Router>
   );
 }
 
-export default App;
+export default MainApp;
+
---
+++ client/src/components/appbar/appbar.component.jsx
@@ -0,0 +1,151 @@
+import React, {useState, useRef, useEffect} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import FilterListIcon from '@material-ui/icons/FilterList';
+import Link from "@material-ui/core/Link";
+//import axios from "axios";
+import {/*Route, Switch,*/ useHistory} from "react-router-dom";
+import classNames from 'classnames';
+import CssBaseline from "@material-ui/core/CssBaseline";
+//Import Menu
+import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Switch from "@material-ui/core/Switch";
+
+
+export const HeaderBar = (props) => {
+    const history = useHistory();
+    const [isOpen, setIsOpen] = useState(false);
+    const { classes, filter, setSearchText, onFilterChange, colorSwitch, darkState, displayAlt} = props;
+    const [open, setOpen] = useState(false);
+    const anchorRef = useRef(null);
+
+    const handleSearchChange = (event) => {
+        setSearchText(event.target.value)
+        console.log("searchText", event.target.value)
+        if (history.location.pathname === '/'){
+          //Check also selected timeline option and redirect to the corespondant path
+          var newPath = localStorage.getItem('timelineContent') || "graphics"
+          history.push("/"+newPath);
+        }
+    }
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    //MENU
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+    const handleClose = (event) => {
+        console.log("handleClose Ev:", event);
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+        setOpen(false);
+    };
+    const handlePaintLink = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+        setOpen(false);
+        history.push("/paintings");
+    };
+    const handleGraphLink = (event) => {
+        console.log("handleClose Ev:", event);
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+        setOpen(false);
+        history.push("/graphics");
+    };
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+          event.preventDefault();
+          setOpen(false);
+        }
+    }
+    const prevOpen = useRef(open);
+    useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+        anchorRef.current.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
+    const {pathname} = history.location
+    const allowedFilterPaths = ["/paintings", "/graphics"]
+    return (
+        <AppBar position="fixed"
+                className={classNames(classes.appBar, {
+                    [classes.appBarShift]: isOpen,
+                })}
+        >
+            <CssBaseline />
+            <Toolbar>
+
+                <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                    <Link href="/" style={{color:'white'}}>
+                        Kryptowaehrung Visualisierung
+                    </Link>
+                </Typography>
+                {/*** Search Box ***/}
+                {
+                <div className={classes.search}>
+                    <div className={classes.searchIcon}>
+                        <SearchIcon/>
+                    </div>
+                    <InputBase
+                        placeholder="Search…"
+                        classes={{
+                            input: classes.inputInput,
+                            root: classes.inputRoot,
+                        }}
+                        inputProps={{'aria-label': 'search'}}
+                        onChange={handleSearchChange}
+                    />
+                </div>}
+                <div className={classes.grow} />
+                {/*** End Search Box ***/}
+                <div>
+                    <Button href="/">
+                        <span className={classes.menuBut}>Startseite</span>
+                    </Button>
+                    <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                    {({ TransitionProps, placement }) => (
+                        <Grow
+                        {...TransitionProps}
+                        style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                        >
+                        <Paper>
+                            <ClickAwayListener onClickAway={handleClose}>
+                            <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                <MenuItem onClick={handlePaintLink}>Gemälde</MenuItem>
+                                <MenuItem onClick={handleGraphLink}>Grafiken</MenuItem>
+                            </MenuList>
+                            </ClickAwayListener>
+                        </Paper>
+                        </Grow>
+                    )}
+                    </Popper>
+                    <Switch checked={darkState} onChange={colorSwitch} title={displayAlt} />
+                </div>
+
+                {/*** Filter ***/}
+                {!isOpen && (allowedFilterPaths.indexOf(pathname) > -1) && <FilterListIcon
+                    className={classNames(classes.menuButton, isOpen && classes.hide)}
+                    onClick={toggle}
+                />}
+                {/*** End Filter ***/}
+            </Toolbar>
+        </AppBar>
+    );
+}
