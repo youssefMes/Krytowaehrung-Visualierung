febaaea94e387afe23e730f720b3eec5002ae6c3
youssefMes <youssef-mestiri@outlook.fr>
2021-09-05 10:57:11 +0100

    clean up code

--- server/index.js
+++ server/index.js
@@ -7,26 +7,7 @@ app.use(cors())
 const port = process.env.PORT || 9000
 
 app.use('/api', routes)
-let interval;
-
-const getApiAndEmit = socket => {
-    // const client = new RESTClient({
-    //     creds: {
-    //         apiKey: "WBES4COG9QL09T2KXOY3" // your cw api key
-    //     }
-    // });
-
-
-// All requests return promises that return the formatted API data.
-//     client.getOHLC('binance', 'btcusdt', {after: '1262365406', before: '1629479006'}).then((response) =>{
-//         let exchanges = response.result;
-//         console.log(response)
-//         socket.emit("FromAPI", response);
-//     });
-    // Emitting a new message. Will be consumed by the client
-};
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
-//server.listen(port, () => console.log(`Listening on port ${port}`));
